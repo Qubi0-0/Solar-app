7b14e6485bdcf99d4735abd4b9efa844792eedc0
mtszpl <matiplonka@gmail.com>
2024-05-18 16:44:50 +0200

    added click position detection

--- Assets/ClickController.cs
+++ Assets/ClickController.cs
@@ -1,20 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClickController : MonoBehaviour
 {
+    private PlayerInput m_playerInput;
+    private InputAction m_pressAction;
+    private Camera m_camera;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        if (Input.touchCount > 0)
-        {
-            Debug.Log("Touch detected. Touch count: " + Input.touchCount);
-        }
-        else
-        {
-            Debug.Log("No touch detected.");
-        }
+        m_playerInput = GetComponent<PlayerInput>();
+        m_pressAction = m_playerInput.actions.FindAction("IA_Press");
+        m_camera = Camera.main;
     }
+
+    private void OnEnable()
+    {
+        m_pressAction.performed += TouchPressed;
+    }
+    
+    private void OnDisabled()
+    {
+        m_pressAction.performed -= TouchPressed;
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector3 position = m_camera.ScreenToWorldPoint(m_pressAction.ReadValue<Vector2>());
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+        }
+
+
+    }
+
 }
