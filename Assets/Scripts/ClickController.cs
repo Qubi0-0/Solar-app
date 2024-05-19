20db317f9b2cfd3720e8e035d774c5225d995b93
mtszpl <matiplonka@gmail.com>
2024-05-20 00:08:21 +0200

    Touch input fixed

--- Assets/Scripts/ClickController.cs
+++ Assets/Scripts/ClickController.cs
@@ -7,37 +7,31 @@ public class ClickController : MonoBehaviour
 {
     private PlayerInput playerInput;
     private InputAction pressAction;
+    private InputAction positionAction;
     private Camera mainCamera;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         pressAction = playerInput.actions.FindAction("IA_Press");
+        positionAction = playerInput.actions.FindAction("IA_Position");
         mainCamera = Camera.main;
     }
 
-    private void OnEnable()
+    private void Update()
     {
-        pressAction.performed += TouchPressed;
-    }
-
-    private void OnDisable()
-    {
-        pressAction.performed -= TouchPressed;
-    }
-
-    private void TouchPressed(InputAction.CallbackContext context)
-    {
-        Vector2 screenPosition = pressAction.ReadValue<Vector2>();
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (pressAction.WasPerformedThisFrame())
         {
-            Planet planet = hit.transform.GetComponent<Planet>();
-            if (planet)
-                planet.Show();
+            Vector2 position = positionAction.ReadValue<Vector2>();
+            Ray ray = mainCamera.ScreenPointToRay(position);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Planet planet = hit.transform.GetComponent<Planet>();
+                if (planet)
+                    planet.Show();
+            }
         }
-
     }
 
 }
